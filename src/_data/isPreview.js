2869313d07000fc4479a66abb05a7a58a4d8cf03
Raphael Höser <raphael@hoeser.info>
2024-09-13 16:06:58 +0200

    correct ors in preview detection
    Signed-off-by: Raphael Höser <raphael@hoeser.info>

--- src/_data/isPreview.js
+++ src/_data/isPreview.js
@@ -1,6 +1,6 @@
 export default (data) =>
   data.eleventy.env.runMode === "serve" ||
-  process.env.BRANCH !== "main" ||
-  process.env.CF_PAGES_BRANCH !== "main";
+  (process.env.BRANCH && process.env.BRANCH !== "main") ||
+  (process.env.CF_PAGES_BRANCH && process.env.CF_PAGES_BRANCH !== "main");
 
 console.log(process.env);
